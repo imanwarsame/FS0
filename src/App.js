029dede0150fc60e9daf95f844afab2425d863ed
imanwarsame <iwarsame38@gmail.com>
2023-01-12 18:05:04 +0000

    part 1 commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+//All content that needs to be rendered in the browser are
+//usually defined as React components (see below aka JS functions)
+const App = () => {
+  const name = 'Kareem Abdul-Jabbar'
+  const age = 75
+  //The below is JSX, not html
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
 
 export default App
