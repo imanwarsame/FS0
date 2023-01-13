0a0acacf5f5ca755f5f2a86ca68af3ab43d3cd57
imanwarsame <iwarsame38@gmail.com>
2023-01-13 19:25:12 +0000

    Exercise 1.4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,4 +1,9 @@
-//Header component
+/**
+ * The Header function takes in a single argument called props, and returns a div with an h1 element
+ * that contains the value of the course property of the props object.
+ * @param props - {course: "Half Stack application development"}
+ * @returns The Header component is being returned.
+ */
 const Header = (props) => {
   console.log(props);
   return (
@@ -10,7 +15,12 @@ const Header = (props) => {
   )
 }
 
-//Part component
+/**
+ * Part is a function that takes in props and returns a div with a p tag that contains the props.part
+ * and props.exercises
+ * @param props
+ * @returns a JSX element.
+ */
 const Part = (props) => {
   console.log(props);
   return (
@@ -22,29 +32,51 @@ const Part = (props) => {
   )
 }
 
-//Content component
+/**
+ * Component that takes an array of objects, and returns an array of JSX Part elements.
+ * @param props
+ * @returns An array of Part components.
+ */
 const Content = (props) => {
   console.log(props);
+  let content = props.parts.map(value => <Part part={value.name} exercises={value.exercises}/>)
   return (
-    <div>
-      <Part part={props.part} exercises={props.exercises}/>
-    </div>
+    content
   )
 }
 
+/**
+ * Component that takes in an array of objects, and returns the sum of the exercises property of each object.
+ * @param props
+ * @returns The sum of the exercises in the course.
+ */
 const Total = (props) => {
   console.log(props);
+  let sum = 0
+  props.parts.forEach(value => {
+    sum += value.exercises;
+  })
+
   return (
     <div>
       <p>
-        Number of exercises {props.sum}
+        Number of exercises {sum}
       </p>
     </div>
   )
 }
 
 
-//Main app component (aka JS function)
+/**
+ * The function App() returns a div that contains the Header, Content, and Total components. 
+ * 
+ * The Header component is passed the course name as a prop. 
+ * 
+ * The Content component is passed the parts array as a prop. 
+ * 
+ * The Total component is passed the parts array as a prop.
+ * @returns The return value of the function is the JSX element that is rendered to the DOM.
+ */
 const App = () => {
   const course = 'Half Stack application development'
   const parts = [
@@ -62,18 +94,11 @@ const App = () => {
     }
   ]
 
-  let sum = 0
-  parts.forEach(value => {
-    sum += value.exercises;
-  })
-
-  let content = parts.map(value => <Content part={value.name} exercises={value.exercises}/>)
-
   return (
     <div>
-      <Header course={course}/>
-      {content}
-      <Total sum={sum}/>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
