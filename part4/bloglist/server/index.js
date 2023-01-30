777c7355d145c1fff6eec7596d53b53155a886f4
imanwarsame <iwarsame38@gmail.com>
2023-01-30 21:44:57 +0000

    Exercise 4.2, refactored app.js and index.js

--- part4/bloglist/server/index.js
+++ part4/bloglist/server/index.js
@@ -1,24 +1,7 @@
-// const app = require('./app') // the actual Express application
+const app = require('./app') // the actual Express application
 const config = require('./utils/config')
-const { info, error } = require('./utils/logger')
-const { PORT, MONGODB_URI } = require('./utils/config')
-const blogsRouter = require('./controllers/blogsRouter')
+const logger = require('./utils/logger')
 
-require('dotenv').config();
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-
-const mongoose = require('mongoose')
-mongoose.set('strictQuery', false);
-mongoose.connect(MONGODB_URI)
-
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
-
-
-app.listen(PORT, () => {
-  info(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
