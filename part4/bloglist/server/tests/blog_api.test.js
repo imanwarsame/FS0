950c9bb59fd84b44d948c9b9107fd33c916feb76
imanwarsame <iwarsame38@gmail.com>
2023-02-02 19:14:49 +0000

    Exercise 4.9

--- part4/bloglist/server/tests/blog_api.test.js
+++ part4/bloglist/server/tests/blog_api.test.js
@@ -2,28 +2,9 @@ const mongoose = require('mongoose');
 const supertest = require('supertest');
 const Blog = require('../models/blog');
 const app = require('../app');
-
+const { initialBlogs, blogsInDB } = require('./test_helper');
 const api = supertest(app);
 
-
-const initialBlogs = [
-	{
-		_id: '5a422a851b54a676234d17f7',
-		title: 'React patterns',
-		author: 'Michael Chan',
-		url: 'https://reactpatterns.com/',
-		likes: 7,
-		__v: 0
-	},
-	{
-		_id: '5a422aa71b54a676234d17f8',
-		title: 'Go To Statement Considered Harmful',
-		author: 'Edsger W. Dijkstra',
-		url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-		likes: 5,
-		__v: 0
-	},
-];
 
 beforeEach(async () => {
 	await Blog.deleteMany({});
@@ -50,6 +31,15 @@ test('blogs are returned as json', async () => {
 		.expect(200)
 		.expect('Content-Type', /application\/json/);
 }, 100000);
+
+
+/* This is a test that checks that the id property exists. */
+test('id property exists', async () => {
+	const allBlogs = await blogsInDB();
+	allBlogs.forEach(blog => {
+		expect(blog.id).toBeDefined();
+	});
+});
 
 
 /* This is a test that checks that all blogs are returned. */
