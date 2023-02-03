1f33f83b9eedd71d9d0100ae2aa90d1d0c4b24f5
imanwarsame <iwarsame38@gmail.com>
2023-02-03 20:02:44 +0000

    Exercise 4.17

--- part4/bloglist/server/models/user.js
+++ part4/bloglist/server/models/user.js
@@ -25,7 +25,7 @@ userSchema.set('toJSON', {
 		returnedObject.id = returnedObject._id.toString();
 		delete returnedObject._id;
 		delete returnedObject.__v;
-		// the passwordHash should not be revealed
+		//The passwordHash should not be revealed
 		delete returnedObject.passwordHash;
 	}
 });
