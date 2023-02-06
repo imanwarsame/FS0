da711092fa7964fce36ffa880735b7f529f9c3f1
imanwarsame <iwarsame38@gmail.com>
2023-02-06 19:55:03 +0000

    Exercise 5.1

--- part5/client/src/App.js
+++ part5/client/src/App.js
@@ -1,10 +1,14 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import blogService from './services/blogs';
+import loginService from './services/login';
+import Notification from "./components/Notification";
+
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
-  const [errorMessage, setErrorMessage] = useState(null);
+  const [userNotification, setUserNotification] = useState(null);
+  const [notificationType, setNotificationType] = useState('notification');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [user, setUser] = useState(null);
@@ -16,32 +20,80 @@ const App = () => {
   }, []);
 
 
-  // const handleLogin = async (event) => {
-  //   event.preventDefault()
+  const handleLogin = async (event) => {
+    event.preventDefault()
     
-  //   try {
-  //     const user = await loginService.login({
-  //       username, password,
-  //     })
-  //     setUser(user);
-  //     setUsername('');
-  //     setPassword('');
-  //   } catch (exception) {
-  //     setErrorMessage('Wrong credentials')
-  //     setTimeout(() => {
-  //       setErrorMessage(null)
-  //     }, 5000);
-  //   }
-  // };
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user);
+      setUsername('');
+      setPassword('');
+    } catch (error) {
+      console.log(error.response.data.error)
+      setNotificationType('errorMsg')
+      setUserNotification(error.response.data.error) //Error notification
+      setTimeout(() => {
+        setUserNotification(null)
+        setNotificationType('notification')
+      }, 5000);
+    }
+  };
 
-  return (
+
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+          <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+          <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>      
+  );
+
+  const blogsForm = () => (
     <div>
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
     </div>
-  )
+  );
+
+  return (
+    <div>
+      <h2>blogs</h2>
+
+      <Notification message={userNotification} notificationType={notificationType}/>
+
+      {!user && loginForm()} 
+      {user && <div>
+        <p>{user.name} logged in</p>
+          {blogsForm()}
+        </div>
+      }
+
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  );
 }
 
 export default App
